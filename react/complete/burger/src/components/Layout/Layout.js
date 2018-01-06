8900a61cae92301b17a0a3cb5877983a88b8ac09
Andrew Burnett <andrew@andburn.info>
2018-01-06 21:51:30 +0000

    react: burger basic

---
+++ react/complete/burger/src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './Layout.module.css';
+
+const layout = (props) => (
+    <React.Fragment>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={styles.content}>
+            {props.children}
+        </main>
+    </React.Fragment>
+);
+
+export default layout;
